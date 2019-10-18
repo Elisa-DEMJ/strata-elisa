f45725eb4abc5f16d00b61f9acce6ab6e9f9e273
Elisa D <dalleauelisa@gmail.com>
2019-10-18 17:33:41 +0200

    up

---
+++ config.php
@@ -0,0 +1,16 @@
+<?php 
+
+
+$twitter = "xxx";
+$githup = "aaa";
+$dribbble = "aaaa";
+$envelope = "bbb";
+$instagram = "mmmm";
+$facebook = "gdjgn";
+$cp = 17000;
+$ville = "La Rochelle";
+$pays = "France";
+
+
+
+?>
