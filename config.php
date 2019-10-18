1e2d3183a4a972c4d2579584513a1ca5d6e2f224
Michael <contact@eewee.fr>
2019-10-18 21:44:52 +0200

    Ajout de valeurs et 1 variable.

--- config.php
+++ config.php
@@ -1,16 +1,14 @@
 <?php 
-
-
-$twitter = "xxx";
-$githup = "aaa";
-$dribbble = "aaaa";
-$envelope = "bbb";
-$instagram = "mmmm";
-$facebook = "gdjgn";
+$twitter = "https://www.google.fr";
+$githup = "https://www.google.fr";
+$dribbble = "https://www.google.fr";
+$envelope = "contact@tld.com";
+$instagram = "https://www.google.fr";
+$facebook = "https://www.google.fr";
+$adress = "55 Av. Albert Einstein";
 $cp = 17000;
 $ville = "La Rochelle";
 $pays = "France";
-
-
+$tel = "01 02 03 04 05";
 
 ?>
